ce9ce413bb3fe1b892cb1fd32e88b9b1777ded11
abhileshweb <abhilesh@webguruz.in>
2022-11-21 12:57:34 +0530

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Chat App ',
+        title: 'Chat App',
         theme: AppTheme().light,
         darkTheme: AppTheme().dark,
         themeMode: ThemeMode.dark,
